c86bc0db0a2de809c34c818a513a42c8508651ec
Josslin VARON <josslinvaron@gmail.com>
2023-08-27 18:42:50 +0200

    Commit init + init H2 database

---
+++ src/main/java/com/azilab/hrassoc/HrassocApplication.java
@@ -0,0 +1,13 @@
+package com.azilab.hrassoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrassocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrassocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azilab/hrassoc/HrassocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azilab.hrassoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrassocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
